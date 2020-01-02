e3727d8428a5f5f8b76eb78aa886c0a9e55c441c
hiukky <developermarsh@gmail.com>
2020-01-02 18:10:03 -0300

    :sparkles: save directories and folders to copy in json

--- scripts/system/configure.sh
+++ scripts/system/configure.sh
@@ -1,6 +1,14 @@
 #! /bin/bash
 source "${BINPATH}/utils/colors.sh"
 
+declare CONFIG=$(<config.json)
+
 _configure(){
   echo "Configuring..."
+
+  for row in "$(echo ${CONFIG} | jq -r '.[]')"; do
+    echo $row | jq '.path'
+  done
 }
+
+_configure
