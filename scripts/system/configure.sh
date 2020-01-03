25db243dcf73dc69c6d9634b29f0d732ccd8a74c
hiukky <developermarsh@gmail.com>
2020-01-02 22:40:09 -0300

    :zap: configure: validate directories

--- scripts/system/configure.sh
+++ scripts/system/configure.sh
@@ -6,8 +6,19 @@ declare CONFIG=$(<config.json)
 _configure(){
   echo "Configuring..."
 
-  for row in "$(echo ${CONFIG} | jq -r '.[]')"; do
-    echo $row | jq '.path'
+  for obj in "$(echo ${CONFIG} | jq -r '.[]')"; do
+    if [[  -n $obj  ]]; then
+      local -a folders=$(echo $obj | jq -r '.folders')
+      folders=(${folders//[\,\"\]\[]/""})
+
+      if [[ -n folders ]]; then
+        for folder in ${folders[@]}; do
+          if [[ -d ~/"$(echo $obj | jq -r '.path')/${folder}" ]]; then
+            echo "Delete old folders in home and copy settings."
+          fi
+        done
+      fi
+    fi
   done
 }
 
