845dfad5b4342e52385489eb2e903dffd20445b8
Romullo <developermarsh@gmail.com>
2021-04-21 20:43:27 -0300

    chore(scripts): package manage improvements

--- .scripts/package.sh
+++ .scripts/package.sh
@@ -2,20 +2,52 @@
 
 set -eE
 
-cd ../.packages
+declare -a ARCH=(
+    nodejs
+    npm
+    yarn
+    thunar
+    tumbler
+    ttf-cascadia-code
+    repo
+    neofetch
+    vagrant
+    net-tools
+    transmission-gtk
+    discord
+    vlc
+    mousepad
+    grim
+    slurp
+)
+
+declare -a AUR=(
+    visual-studio-code-bin
+    figma-linux
+    insomnia
+    stacer
+    beekeeper-studio-bin
+    slack-desktop
+    telegram-desktop-bin
+    grimshot
+    wl-clipboard
+)
+
+declare -a NPM=(
+    vercel
+    npm-check-updates
+    yalc
+)
 
 printf "\n\nPACKAGES"
 
 printf "\n\nInstalling Archlinux packages ... \n\n"
-sudo pacman -S $(cat "arch") --noconfirm --needed
+sudo pacman -S ${ARCH[@]} --noconfirm --needed
 
 printf "\n\nInstalling AUR packages ... \n\n"
-yay -S $(cat "aur") --noconfirm
-
-printf "\n\nInstalling Snap packages ... \n\n"
-sudo snap install $(cat "snap")
+yay -S ${AUR[@]} --noconfirm
 
 printf "\n\nInstalling NPM packages ... \n\n"
-sudo npm install -g $(cat "npm")
+sudo npm install -g ${NPM[@]}
 
 printf "\n\nDone! \n\n"
